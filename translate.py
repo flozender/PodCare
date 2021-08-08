012b4f1c8158524c80e2846be33dd7fc5bf13792
Muntaser Syed <jemsbhai@gmail.com>
2021-08-08 08:13:37 -0400

    Create translate.py
    translation backend

---
+++ translate.py
@@ -0,0 +1,104 @@
+import os
+import pymongo
+import json
+import random
+# import psycopg2
+import hashlib
+import time
+
+
+from hashlib import sha256
+
+
+def translate_text(target, text):
+    """Translates text into the target language.
+
+    Target must be an ISO 639-1 language code.
+    See https://g.co/cloud/translate/v2/translate-reference#supported_languages
+    """
+    # import six
+    from google.cloud import translate_v2 as translate
+
+    translate_client = translate.Client()
+
+    # if isinstance(text, six.binary_type):
+    #     text = text.decode("utf-8")
+
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    result = translate_client.translate(text, target_language=target)
+
+    # print(u"Text: {}".format(result["input"]))
+    # print(u"Translation: {}".format(result["translatedText"]))
+    # print(u"Detected source language: {}".format(result["detectedSourceLanguage"]))
+
+    return result["translatedText"]
+
+
+def hashthis(st):
+
+
+    hash_object = hashlib.md5(st.encode())
+    h = str(hash_object.hexdigest())
+    return h
+
+
+
+
+
+
+def dummy(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    if request.method == 'OPTIONS':
+        # Allows GET requests from origin https://mydomain.com with
+        # Authorization header
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Max-Age': '3600',
+            'Access-Control-Allow-Credentials': 'true'
+        }
+        return ('', 204, headers)
+
+    # Set CORS headers for main requests
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true'
+    }
+
+    request_json = request.get_json()
+
+    
+    mongostr = os.environ.get('MONGOSTR')
+
+    fromLan = request_json['from']
+    toLan = request_json['to']
+    text = request_json['text']
+
+    result = translate_text(toLan, text)
+
+    retjson = {}
+
+    retjson['result'] = result
+
+    return json.dumps(retjson)
+
+
+
+
+    retstr = "action not done"
+
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    else:
+        return retstr
